385b9ffb1465d80957034b4b3e33700867e2ea87
eakyel <e.akyel@outlook.com>
2022-05-26 22:23:44 +0300

    Update component for rendering algortihms

--- components/Algorithms.jsx
+++ components/Algorithms.jsx
@@ -1,19 +1,45 @@
 import AlgorithmCard from "./AlgorithmCard";
 
 export default function Algorithms({ posts }) {
+  const algorithmCategories = Array.from(
+    new Set(posts.map((algorithm) => algorithm.category))
+  );
+
+  const getAlgorithms = (categoryName) => {
+    return posts
+      .filter((algorithm) => algorithm.category === categoryName)
+      .map((algorithm) => (
+        <AlgorithmCard
+          key={algorithm.title.split(" ").join("")}
+          slug={algorithm.slug}
+          title={algorithm.title}
+          category={algorithm.category}
+          timeComplexity={algorithm.timeComplexity || {}}
+        />
+      ));
+  };
+
+  const renderSection = (categoryName, index) => {
+    return (
+      <section key={`${categoryName}-${index}`}>
+        <>
+          <header>
+            <h2 className="text-3xl font-bold mb-12">{categoryName}</h2>
+          </header>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-32">
+            {getAlgorithms(categoryName)}
+          </div>
+        </>
+      </section>
+    );
+  };
+
   return (
-    <section>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-32">
-        {posts.map((post) => (
-          <AlgorithmCard
-            key={post.slug}
-            title={post.title}
-            category={post.category}
-            timeComplexity={post.timeComplexity || {}}
-            slug={post.slug}
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      {algorithmCategories.map((categoryName, index) =>
+        renderSection(categoryName, index)
+      )}
+    </>
   );
 }
