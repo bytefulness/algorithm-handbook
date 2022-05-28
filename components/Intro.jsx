16a61adc60ee6a9741121100037293e47a8ce9e5
eakyel <e.akyel@outlook.com>
2022-05-28 21:53:01 +0300

    Update intro

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from "../lib/constants";
 export default function Intro() {
   return (
     <section className="text-center max-w-5xl mx-auto mt-10 md:mt-36 mb-32">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight mb-4">
+      <h1 className="font-extrabold tracking-tighter text-transparent text-6xl md:text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 pb-4">
         {CMS_NAME}
       </h1>
       <p className="text-xl md:text-2xl md:leading-9 font-light text-gray-500 mb-4">
